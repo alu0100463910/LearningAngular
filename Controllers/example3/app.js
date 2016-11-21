30236e9cdd0bc9f75441efd86426f9b5900adc93
Mendoza Bello <fabio.mendoza@atos.net>
2016-11-21 17:21:11 +0000

    Sharing Data between controllers

---
+++ Controllers/example3/app.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.factory("Share", function(){
+    return {
+        dataObject :{color:"Blue"}
+    }
+});
+
+myApp.controller('ACtrl', function($scope,Share){
+    //$scope.dataObject = {color : "Red"}
+    $scope.dataObject = Share.dataObject;
+});
+
+
+myApp.controller('BCtrl', function($scope,Share){
+    //$scope.dataObject = {color : "Green"}
+    $scope.dataObject=Share.dataObject;
+});
+
