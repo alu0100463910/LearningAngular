3ea42d461829ddb709f614191ccc54c272dd934c
Mendoza Bello <fabio.mendoza@atos.net>
2016-11-22 12:18:49 +0000

    Taking Advantage of Angular JS Provided CSS Classes

---
+++ Output Formatting/example1/app.js
@@ -0,0 +1,12 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('appController', function($scope){
+
+    $scope.value="Im a scope variable named value.";
+
+    $scope.userInput='';
+});
+
+    
+    
+    
