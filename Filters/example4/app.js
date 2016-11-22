7bb4e666e6c40a506f1ec4350bc038bcb294baa0
Mendoza Bello <fabio.mendoza@atos.net>
2016-11-22 10:38:07 +0000

    Using the number and json Filters

---
+++ Filters/example4/app.js
@@ -0,0 +1,27 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('myCtrl', function($scope){
+
+    $scope.model = {
+        'firstName' : 'Fabio',
+        'lastName'  : 'Mendoza',
+        'dob'       :{
+            'month':'February',
+            'day':'1',
+            'year':'1981'
+        }
+    };
+});
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
