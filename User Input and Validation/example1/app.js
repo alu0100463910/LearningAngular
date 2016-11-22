f69618be625bab649b50489c120cef360cb2965e
Mendoza Bello <fabio.mendoza@atos.net>
2016-11-22 11:04:58 +0000

    Working with User Text Input

---
+++ User Input and Validation/example1/app.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('appController', function($scope){
+
+    $scope.model = {
+        firstName:'Fabio',
+        lastName:'Mendoza'
+    };
+});
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
