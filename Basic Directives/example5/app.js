9f3c02da92cfe6232b5739854a991d3742f84e73
Mendoza Bello <fabio.mendoza@atos.net>
2016-11-28 15:24:55 +0000

    Working with Nested Variable Scopes

---
+++ Basic Directives/example5/app.js
@@ -0,0 +1,15 @@
+var myApp = angular.module('myApp',[]);
+
+
+
+myApp.controller('ACtrl', function($scope){
+   
+ //   $scope.value = "Value from A Controller.";
+});
+
+myApp.controller('BCtrl', function($scope){
+    $scope.value = "Value from B Controller.";
+});
+    
+    
+    
